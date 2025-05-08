5bceb14fc6930de069dd8dc9d47fd73a5c7957ab
RaedYassin <raedyassin02468@gmail.com>
2025-05-08 15:11:55 +0300

    change sameSite in token to none

--- utils/createJWT.js
+++ utils/createJWT.js
@@ -6,10 +6,10 @@ const generateJWT = (res,userId) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_EMV !== "develpment",
-    sameSite: "strict",
+    // sameSite: "strict",
     // You need to change sameSite to "none" because your frontend and backend are 
     // on different domains, which is called a cross- site request.
-    // sameSite: "none",
+    sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000
   })
 }
